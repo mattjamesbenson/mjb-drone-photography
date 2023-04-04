157cd869d117592cfb7decfd57bebedd881bc521
Matt James Benson <mattbenson04@googlemail.com>
2023-04-04 21:27:58 +0100

    Initial layout added including header, footer and content space

---
+++ resources/views/front/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <div class="row">
+        <div class="col-md-12">
+            
+        </div>
+    </div>
+</div>
---
+++ resources/views/front/header.blade.php
@@ -0,0 +1,7 @@
+<div class="header">
+    <div class="row">
+        <div class="col-md-12">
+            
+        </div>
+    </div>
+</div>
